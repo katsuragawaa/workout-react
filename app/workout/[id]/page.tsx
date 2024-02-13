16d88890af77ac732e6ce982492066e3d146bbd2
katsuragawaa <andre.katsuragawa@gmail.com>
2024-02-12 22:41:44 -0300

    Add navigation and back button to workout page

--- app/workout/[id]/page.tsx
+++ app/workout/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { ExerciseCard } from "@/components/exercise-card";
+import { buttonVariants } from "@/components/ui/button";
 import { exercises, workouts } from "@/lib/workout-mock";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 type WorkoutPageProps = {
   params: { id: string };
@@ -10,14 +13,22 @@ export default function WorkoutPage({ params }: WorkoutPageProps) {
   const workout = workouts.find((workout) => workout.id === Number(id)) || { title: "Workout not found" };
 
   return (
-    <main className="container flex min-h-screen max-w-2xl flex-col">
-      <h1 className="pb-10 pt-14 text-5xl font-extrabold">{workout.title}</h1>
+    <>
+      <nav className="container max-w-4xl pt-14">
+        <Link className={buttonVariants({ variant: "outline", size: "icon" })} href="/">
+          <ArrowLeft className="h-4 w-4" />
+        </Link>
+      </nav>
 
-      <div className="grid grid-cols-2 gap-3">
-        {exercises.map((exercise) => (
-          <ExerciseCard key={exercise.id} exercise={exercise} />
-        ))}
-      </div>
-    </main>
+      <main className="container flex min-h-screen max-w-2xl flex-col">
+        <h1 className="pb-10 pt-14 text-5xl font-extrabold">{workout.title}</h1>
+
+        <div className="grid grid-cols-2 gap-3">
+          {exercises.map((exercise) => (
+            <ExerciseCard key={exercise.id} exercise={exercise} />
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
