edcf27f5f2ba7c18c4aea5bc37df47942ed09c26
katsuragawaa <andre.katsuragawa@gmail.com>
2024-02-12 23:21:41 -0300

    Update workout title in workout-mock.ts

--- lib/workout-mock.ts
+++ lib/workout-mock.ts
@@ -2,7 +2,7 @@ export const workouts = [
   { id: 1, title: "Treino A", description: "Peito, ombros, triceps e abs" },
   { id: 2, title: "Treino B", description: "Costas, lombar e biceps" },
   { id: 3, title: "Treino C", description: "Pernas e glúteos" },
-  { id: 4, title: "Treino D", description: "Glúteos intensos" },
+  { id: 4, title: "Treino De Costas com Peitoral", description: "Glúteos intensos" },
 ];
 
 export const exercises = [
