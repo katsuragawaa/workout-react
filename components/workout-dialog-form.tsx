b3ce0d713d5a9a2c9b9c87cab114d2d0fcc6eedc
katsuragawaa <andre.katsuragawa@gmail.com>
2024-02-14 20:47:11 -0300

    feat: wip form

---
+++ components/workout-dialog-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+});
+
+export function WorkoutDialogForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    setIsOpen(false);
+  }
+
+  function openDialog() {
+    setIsOpen(true);
+  }
+
+  function closeDialog() {
+    setIsOpen(false);
+  }
+
+  function toggleDialog(open: boolean) {
+    open ? setIsOpen(true) : setIsOpen(false);
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={toggleDialog}>
+      <DialogTrigger asChild>
+        <Button className="w-fll mt-10" onClick={openDialog}>
+          Novo workout
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-6">
+              <div>
+                <h3 className="text-lg font-medium">Workout</h3>
+                <p className="text-sm text-muted-foreground">Plan your workout.</p>
+              </div>
+
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Título</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Treino A" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Salvar</Button>
+              <Button type="button" variant="secondary" onClick={closeDialog}>
+                Fechar
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
