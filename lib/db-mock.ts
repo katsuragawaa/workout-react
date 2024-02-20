85db43a5fe56ecf6e9772d5ca51f2bde279e0435
katsuragawaa <andre.katsuragawa@gmail.com>
2024-02-19 22:26:09 -0300

    Add mock database implementation

---
+++ lib/db-mock.ts
@@ -0,0 +1,81 @@
+type Workout = {
+  id: number;
+  name: string;
+};
+
+type Exercise = {
+  id: number;
+  name: string;
+  muscle: string;
+  sets: number;
+  reps: number;
+  weight: number;
+  workoutId: number;
+};
+
+const workouts: Workout[] = [];
+const exercises: Exercise[] = [];
+let lastWorkoutId = 0;
+let lastExerciseId = 0;
+
+const getWorkouts = () => {
+  return workouts;
+};
+
+const getExercises = () => {
+  return exercises;
+};
+
+const getExercisesByWorkout = (workoutId: number) => {
+  return exercises.filter((exercise) => exercise.workoutId === workoutId);
+};
+
+const saveWorkout = (workout: Omit<Workout, "id">) => {
+  const newWorkout = { ...workout, id: ++lastWorkoutId };
+  workouts.push(newWorkout);
+};
+
+const saveExercise = (exercise: Omit<Exercise, "id">) => {
+  const newExercise = { ...exercise, id: ++lastExerciseId };
+  exercises.push(newExercise);
+};
+
+const updateWorkoutById = (workoutId: number, updatedWorkout: Partial<Workout>) => {
+  const workoutIndex = workouts.findIndex((workout) => workout.id === workoutId);
+  if (workoutIndex !== -1) {
+    workouts[workoutIndex] = { ...workouts[workoutIndex], ...updatedWorkout };
+  }
+};
+
+const updateExerciseById = (exerciseId: number, updatedExercise: Partial<Exercise>) => {
+  const exerciseIndex = exercises.findIndex((exercise) => exercise.id === exerciseId);
+  if (exerciseIndex !== -1) {
+    exercises[exerciseIndex] = { ...exercises[exerciseIndex], ...updatedExercise };
+  }
+};
+
+const deleteWorkoutById = (workoutId: number) => {
+  const index = workouts.findIndex((workout) => workout.id === workoutId);
+  if (index !== -1) {
+    workouts.splice(index, 1);
+  }
+};
+
+const deleteExerciseById = (exerciseId: number) => {
+  const index = exercises.findIndex((exercise) => exercise.id === exerciseId);
+  if (index !== -1) {
+    exercises.splice(index, 1);
+  }
+};
+
+export {
+  saveExercise as addExercise,
+  saveWorkout as addWorkout,
+  deleteExerciseById,
+  deleteWorkoutById,
+  getExercises,
+  getExercisesByWorkout,
+  getWorkouts,
+  updateExerciseById as editExerciseById,
+  updateWorkoutById as editWorkoutById,
+};
